ae06d3abf228d8406b36da8881d0e6a2033be27f
hachnv8 <hachnv8@gmail.com>
2023-05-11 09:14:21 +0700

    add listnode structure

---
+++ src/leetcode/ListNode.java
@@ -0,0 +1,12 @@
+package leetcode;
+
+/**
+ * Created by HachNV on 25/04/2023
+ */
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val;}
+    ListNode(int val, ListNode next) { this.val = val; this.next = next;}
+}
